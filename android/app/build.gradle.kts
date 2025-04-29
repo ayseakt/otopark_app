1af3119c18bac4d40a76b66dc8eadd1754cd92a1
ayseakt <ayseaktug003@gmail.com>
2025-04-29 19:20:12 +0300

    harita

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+    
 }
 
 android {
@@ -42,4 +44,15 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // 🔗 Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+
+    // 🔥 Firestore desteği
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
+    // (İsteğe Bağlı) Firebase Analytics
+    implementation("com.google.firebase:firebase-analytics")
 }
